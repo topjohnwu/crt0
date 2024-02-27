ba8166a48cf9424d0fb46691c64467c20da7cdfe
topjohnwu <topjohnwu@gmail.com>
2024-02-26 17:33:58 -0800

    Add all required flags into Android.mk

--- Android.mk
+++ Android.mk
@@ -3,10 +3,13 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := crt0
 
+# -lc -lm is hardcoded in this variable, disable it
+TARGET_LDLIBS :=
+
 # Manually link the compiler runtime library
 LOCAL_compiler_rt := $(shell $(TARGET_CC) -target $(LLVM_TRIPLE)$(TARGET_PLATFORM_LEVEL) --print-libgcc-file-name)
 
-LOCAL_EXPORT_LDFLAGS := -static -nostartfiles -nodefaultlibs $(LOCAL_compiler_rt) -Wl,--error-limit=0
+LOCAL_EXPORT_LDFLAGS := -static -nostartfiles -nodefaultlibs $(LOCAL_compiler_rt) -Wl,-wrap,abort_message
 LOCAL_CFLAGS := -Wno-c99-designator
 
 LOCAL_SRC_FILES := \
